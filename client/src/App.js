010e686ad84b636cbd82f5dd07446111c47f8793
Syed Shah <syed.shah@nyu.edu>
2017-11-21 16:40:08 -0500

    Added some components

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Header = (props) => {
+  return (
+    <header>
+      <div className='header'>
+        Squares.io
+      </div>
+    </header>
+  )
+}
+
+
+const Footer = (props) => {
+  return (
+    <footer>
+      Lauren | Syed | Abrian
+    </footer>
+  )
+}
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Header />
+
+      <Footer />
       </div>
     );
   }
